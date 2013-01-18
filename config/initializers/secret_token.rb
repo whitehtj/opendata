771ecd93fdb9b87ae5134f2fe6c25abac1b5c48e
Terence Rich Whitehead <whitehtj@btopenworld.com>
2013-01-18 15:57:27 +0000

    First push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpendataTemp001::Application.config.secret_token = '1cefad935023c814672e6cd27ee08e16bff3056e09c8f31630b783a4d2f3b783759ce5842834b498037e054ca1e9d6a703e53a17a2e9a26f2e054d417dfe8843'
